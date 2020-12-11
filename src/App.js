4acb5e67b25b6cb85a754f83a5b7784f1d0f75c2
jtreeves <jackson.t.reeves@gmail.com>
2020-12-11 15:27:26 -0500

    [components] Add files for Weather and WeatherResult

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Home from './components/Home'
+import Contact from './components/Contact'
+import Projects from './components/Projects'
+import ProjectDetails from './components/ProjectDetails'
+import Navigation from './components/Navigation'
+
 import './App.css'
 
 function App() {
+    const projectsList = [
+        {
+            id: 1,
+            name: 'Matches and Patches',
+            url: '/',
+            description: 'Very painful'
+        },
+        {
+            id: 2,
+            name: 'Read This Next',
+            url: '/',
+            description: 'Absolutely awful'
+        }
+    ]
+    
+    
     return (
         <div className='App-header'>
             Portfolio
--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -1,1 +1,5 @@
-js
+function ProjectDetails() {
+
+}
+
+export default ProjectDetails
